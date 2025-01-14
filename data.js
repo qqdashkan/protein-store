4c571396bf06824ce078512b5270c3210a78c92e
Daria <qqdashkan@icloud.com>
2025-01-15 00:58:33 +0200

    protein shop main page

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ export const data = [
   {
     id: 1,
     title: "Peanut Butter Smooth Crunchy",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€2.85",
     description:
       "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
@@ -10,7 +10,7 @@ export const data = [
   {
     id: 2,
     title: "Protein Bar 33%",
-    item_URL: "/src/assets/bar-salted-caramel.png",
+    item_URL: "/bar-salted-caramel.png",
     price: "€7.92",
     description:
       "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
@@ -18,7 +18,7 @@ export const data = [
   {
     id: 3,
     title: "Whey Protein",
-    item_URL: "/src/assets/pump-reactor.png",
+    item_URL: "/pump-reactor.png",
     price: "€8.53",
     description:
       "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.\n\nQuisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
@@ -26,7 +26,7 @@ export const data = [
   {
     id: 4,
     title: "Creatine",
-    item_URL: "/src/assets/whey-coconut.png",
+    item_URL: "/whey-coconut.png",
     price: "€2.25",
     description:
       "Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
@@ -34,7 +34,7 @@ export const data = [
   {
     id: 5,
     title: "Protein Bar 33%",
-    item_URL: "/src/assets/bar-strawberry.png",
+    item_URL: "/bar-strawberry.png",
     price: "€6.60",
     description:
       "Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
@@ -42,7 +42,7 @@ export const data = [
   {
     id: 6,
     title: "Whey Protein",
-    item_URL: "/src/assets/whey-vanilla.png",
+    item_URL: "/whey-vanilla.png",
     price: "€4.23",
     description:
       "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
@@ -50,7 +50,7 @@ export const data = [
   {
     id: 7,
     title: "Fixflex",
-    item_URL: "/src/assets/bcaa-pink-grapefruit.png",
+    item_URL: "/bcaa-pink-grapefruit.png",
     price: "€0.11",
     description:
       "Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.\n\nProin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
@@ -58,7 +58,7 @@ export const data = [
   {
     id: 8,
     title: "Protein bar 33%",
-    item_URL: "/src/assets/bcaa-apple-cherry.png",
+    item_URL: "/bcaa-apple-cherry.png",
     price: "€3.62",
     description:
       "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
@@ -66,7 +66,7 @@ export const data = [
   {
     id: 9,
     title: "Domainer",
-    item_URL: "/src/assets/bcaa-cola.png",
+    item_URL: "/bcaa-cola.png",
     price: "€4.05",
     description:
       "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
@@ -74,7 +74,7 @@ export const data = [
   {
     id: 10,
     title: "Konklux",
-    item_URL: "/src/assets/whey-strawberry.png",
+    item_URL: "/whey-strawberry.png",
     price: "€8.76",
     description:
       "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.",
@@ -82,7 +82,7 @@ export const data = [
   {
     id: 11,
     title: "Zaam-Dox",
-    item_URL: "/src/assets/whey-yoghurt.png",
+    item_URL: "/whey-yoghurt.png",
     price: "€1.14",
     description:
       "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.",
@@ -90,7 +90,7 @@ export const data = [
   {
     id: 12,
     title: "Creatine",
-    item_URL: "/src/assets/creatine.png",
+    item_URL: "/creatine.png",
     price: "€4.02",
     description:
       "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
@@ -98,7 +98,7 @@ export const data = [
   {
     id: 13,
     title: "Voyatouch",
-    item_URL: "/src/assets/bcaa-tropical.png",
+    item_URL: "/bcaa-tropical.png",
     price: "€7.37",
     description:
       "Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
@@ -106,7 +106,7 @@ export const data = [
   {
     id: 14,
     title: "Zamit",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€6.38",
     description:
       "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.\n\nDuis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
@@ -114,7 +114,7 @@ export const data = [
   {
     id: 15,
     title: "Cardify",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€0.51",
     description:
       "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.\n\nFusce consequat. Nulla nisl. Nunc nisl.",
@@ -122,7 +122,7 @@ export const data = [
   {
     id: 16,
     title: "Treeflex",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€8.30",
     description:
       "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
@@ -130,28 +130,28 @@ export const data = [
   {
     id: 17,
     title: "Stringtough",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€6.38",
     description: "Peanut Butter Smooth Crunchy 1 kg",
   },
   {
     id: 18,
     title: "Lotlux",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€8.41",
     description: "Peanut Butter Smooth Crunchy 500g",
   },
   {
     id: 19,
     title: "Cookley",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€0.42",
     description: "Peanut Butter Smooth Crunchy 500g",
   },
   {
     id: 20,
     title: "Alphazap",
-    item_URL: "/src/assets/peanut-butter.png",
+    item_URL: "/peanut-butter.png",
     price: "€3.55",
     description:
       "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
